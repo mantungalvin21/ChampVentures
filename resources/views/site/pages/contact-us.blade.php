c2e3405dc0ee971443b88661a9b6ff8f8cb33ce2
mantungalvin21 <98724589+mantungalvin21@users.noreply.github.com>
2024-08-13 20:45:06 +0800

    wip

--- resources/views/site/pages/contact-us.blade.php
+++ resources/views/site/pages/contact-us.blade.php
@@ -117,10 +117,7 @@
                 </div>
                 <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
                     <div class="h-100" style="min-height: 400px;">
-                        <iframe class="rounded w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                        frameborder="0" allowfullscreen="" aria-hidden="false"
-                        tabindex="0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52953.65250758915!2d-84.04006250689078!3d33.951330336677096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f508ebdb5f57ab%3A0xc3db0f7dcd03069a!2sLawrenceville%2C%20GA%2C%20USA!5e0!3m2!1sen!2sph!4v1723549895801!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
